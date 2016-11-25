22fade623a37369fa5c51cfe84bfa11b06a16b34
beduin90 <maciej.niezabitowski@student.uj.edu.pl>
2016-11-25 19:31:48 +0100

    four commit

--- src/test/java/com/inspol/tests/testPage.java
+++ src/test/java/com/inspol/tests/testPage.java
@@ -3,10 +3,10 @@ package com.inspol.tests;
 import org.junit.Test;
 
 
-public class testPage extends TestBase {
+public class testPage  extends TestBase {
 
     @Test
-    public void testPage_01() throws InterruptedException {
+    public void testPage_01()  throws InterruptedException {
         System.out.println("test2");
         Thread.sleep(500);
     }
